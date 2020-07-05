4ca590831210dcbb7dbcaed55d379cf9d2e9c8ea
stephan lai hsu <stephan.lai.hsu@gmail.com>
2020-07-04 23:07:30 -0300

    Usando shinydashboard

---
+++ ShinyDashboard/R/mod_penguins.R
@@ -0,0 +1,45 @@
+library(shiny)
+
+# shinymod -> rascunho de um módulo shiny com o básico - Jeito antigo
+mod_penguins_UI <- function(id) {
+
+  ns <- NS(id)
+
+  vars_num <- palmerpenguins::penguins %>%
+    select(where(is.numeric)) %>%
+    names()
+
+  tagList(
+    h1("Base penguins"),
+    fluidRow(
+      column(
+        width = 4,
+        selectInput(
+          inputId = ns("variavel"),
+          label = "Selecione uma variável",
+          choices = vars_num
+        )
+      ),
+      column(
+        width = 8,
+        plotOutput(ns("grafico"))
+      )
+    )
+  )
+}
+
+mod_penguins_server <- function(id) {
+  shiny::moduleServer(
+    id,
+    function(input, output, session){
+
+      output$grafico <- renderPlot({
+
+        hist(palmerpenguins::penguins[,input$variavel,
+                                      drop = TRUE])
+
+      })
+
+    }
+  )
+}
